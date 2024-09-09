52df7916fe6e542adfcab54a71566122d328425a
buttersays <naaraujolongo@gmail.com>
2024-09-08 22:47:08 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,25 @@
+let dados = [
+    {
+        titulo: "pombinha branca",
+        descricao: "A Pomba Branca: Símbolo Universal de Paz e Esperança. A pomba branca, com sua plumagem imaculada e voos elegantes, é um símbolo carregado de significado em diversas culturas e religiões ao redor do mundo.",
+        link: "https://i.pinimg.com/236x/33/fd/d2/33fdd206ae03ead3a21b3bc7e8a99d50.jpg",
+        tags: "paz bonitinha solta",
+    },
+    {
+        titulo: "pomba preta",
+        descricao: "A pomba preta, com sua plumagem escura e enigmática, desperta curiosidade e interpretações variadas. Ao contrário de sua    contraparte branca, a pomba preta é frequentemente associada a mistérios, transformações e, em algumas culturas, até mesmo a aspectos mais sombrios da vida.",
+        link: "https://img.freepik.com/fotos-premium/pombo-correio-de-pena-preta-isolado-fundo-branco_34013-1122.jpg",
+        tags: "sé estranha praia",
+    },
+    {
+        titulo: "pombinha nenem",
+        descricao: "Observe a ternura dos pombos filhotes! Pequenos e indefesos, eles dependem totalmente de seus pais para se alimentarem e crescerem. Descubra mais sobre o desenvolvimento desses pássaros e a importância do cuidado parental em seu crescimento.",
+        link: "https://segredosdomundo.r7.com/wp-content/uploads/2016/03/5-6.jpg",
+        tags: "escondido escondida sem pena"
+    },
+    {   titulo: "pombinha enroladinha",
+        descricao: "Com sua plumagem única e exuberante, a pomba cacheada é um verdadeiro espetáculo da natureza. Suas penas, delicadamente encaracoladas, criam um efeito visual encantador e a diferenciam de outras raças de pombos. Essa ave ornamental é apreciada por sua beleza singular e por sua natureza dócil.",
+        link: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjyXAKxtvHHfmM6jCr4WQwKUGLsG__owN3EWZKH6MMtBFQnPE1bQ5rt8e-zysLBv_k2BbfKRsTm6NI9DZ8GcdZzNl4KykJtG0KAu8YrhCaCOlGHFJjqRJgAAH5acrf31OxF8GispO9QExg/s640/pc+Frillback.jpg",
+        tags: "encaracolada enrolada cacheada"
+    }
+];
